8e54aff56a17a043fefeb59e352fc3ea86b47665
Gekoga <pdv2005@bk.ru>
2024-11-02 03:26:35 +0300

    added redisStore

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ import { createServer } from 'http'
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
+var RedisStore = require('connect-redis')(session);
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
